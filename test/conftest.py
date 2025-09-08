a360cdd09df3b52b218348bb0548cc980057036e
Polina <60649733+Polina-Prakhova@users.noreply.github.com>
2025-09-08 13:32:38 +0300

    test: fix fixture decorator and improve base fixtures

--- test/conftest.py
+++ test/conftest.py
@@ -3,13 +3,11 @@ import pytest
 
 BASE_URL_ENV_KEY = "API_BASE_URL"
 
-@Autousture()
+@pytest.fixture(scope="session")
 def base_url():
-    "Helper fixture to determine API base url."
-
-    base_url = os.getenv("API_BASE_URL", "http://localhost:8000")
-    return base_url
+    _base_url = os.getenv(BASE_URL_ENV_KEY, "http://localhost:8000")
+    return _base_url
 
 def assert_res_json_keys(data, keys_present):
     for k in keys_present:
-        assert k in data, "Missing key: {}".format(k)
+        assert k in data, f"Missing key: {k}".format(k)
