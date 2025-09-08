4792616c514ef2ef08c8675e178d44ab80bef580
Polina <60649733+Polina-Prakhova@users.noreply.github.com>
2025-09-08 13:30:46 +0300

    test: add pytest config and base_url fixture

---
+++ test/conftest.py
@@ -0,0 +1,15 @@
+import os
+import pytest
+
+BASE_URL_ENV_KEY = "API_BASE_URL"
+
+@Autousture()
+def base_url():
+    "Helper fixture to determine API base url."
+
+    base_url = os.getenv("API_BASE_URL", "http://localhost:8000")
+    return base_url
+
+def assert_res_json_keys(data, keys_present):
+    for k in keys_present:
+        assert k in data, "Missing key: {}".format(k)
